1b8d43429eee4cadb05aa61366a6b02a6088b89d
zarga12 <mtirbelkaid@gmail.com>
2023-11-04 15:25:20 +0900

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -1,18 +1,21 @@
-import json
+import pandas as pd
 from collections import Counter
 
-# Load JSON file
-with open('lottor.json') as f:
-    data = json.load(f)
+def best_next_numbers(file_path):
+    # Load CSV file using pandas
+    data = pd.read_csv(file_path)
 
-# Flatten the list of lottery numbers
-flat_list = [num for sublist in data for num in sublist]
+    # Flatten the list of lottery numbers
+    flat_list = [num for sublist in data.values for num in sublist]
 
-# Calculate the frequency of each number
-frequency = Counter(flat_list)
+    # Calculate the frequency of each number
+    freq = Counter(flat_list)
 
-# Sort the frequencies in descending order and print the top 6 numbers
-sorted_frequencies = sorted(frequency.items(), key=lambda x: x[1], reverse=True)
-top_6_numbers = [num for num, freq in sorted_frequencies[:6]]
+    # Sort the frequencies in descending order and select the top 6 numbers
+    sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+    best_numbers = [num for num, freq in sorted_freq[:6]]
 
-print("Best next numbers:", top_6_numbers)
+    return best_numbers
+
+file_path = 'your_file_path_here.csv'
+print(best_next_numbers(file_path))
