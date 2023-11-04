b18c643803b149de82218d4f1a171c18fe7c7322
zarga12 <mtirbelkaid@gmail.com>
2023-11-04 15:22:49 +0900

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -1,37 +1,18 @@
-import pandas as pd
+import json
+from collections import Counter
 
-def analyze_lotto_numbers(lotto_numbers_file):
-  """Analyzes lotto numbers from a CSV file.
+# Load JSON file
+with open('lottor.json') as f:
+    data = json.load(f)
 
-  Args:
-    lotto_numbers_file: The path to the CSV file containing the lotto numbers.
+# Flatten the list of lottery numbers
+flat_list = [num for sublist in data for num in sublist]
 
-  Returns:
-    A Pandas DataFrame containing the analysis results.
-  """
+# Calculate the frequency of each number
+frequency = Counter(flat_list)
 
-  # Load the lotto numbers from the CSV file.
-  lotto_numbers = pd.read_csv(lotto_numbers_file)
+# Sort the frequencies in descending order and print the top 6 numbers
+sorted_frequencies = sorted(frequency.items(), key=lambda x: x[1], reverse=True)
+top_6_numbers = [num for num, freq in sorted_frequencies[:6]]
 
-  # Calculate the frequency of each number.
-  lotto_numbers_frequency = lotto_numbers['number'].value_counts()
-
-  # Calculate the trend of each number.
-  lotto_numbers_trend = lotto_numbers['number'].diff()
-
-  # Calculate the correlation between each number and other numbers.
-  lotto_numbers_correlation = lotto_numbers.corr()
-
-  # Return the analysis results.
-  return pd.DataFrame({
-    'number': lotto_numbers['number'],
-    'frequency': lotto_numbers_frequency,
-    'trend': lotto_numbers_trend,
-    'correlation': lotto_numbers_correlation.to_numpy()
-  })
-
-# Analyze the lotto numbers from the CSV file.
-lotto_numbers_analysis = analyze_lotto_numbers('lotto_numbers.csv')
-
-# Print the analysis results.
-print(lotto_numbers_analysis)
+print("Best next numbers:", top_6_numbers)
